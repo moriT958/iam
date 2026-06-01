f5755963bd2323ee510bf9f9d80ea77b7cba8371
Morita <154768101+moriT958@users.noreply.github.com>
2026-06-02 01:18:36 +0900

    feat: Post detail page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import rehypeHighlight from "rehype-highlight";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [rehypeHighlight],
+  },
+});
