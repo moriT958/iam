cf80ecf7d62b76ab720012558b3e528fb3a10147
Morita <154768101+moriT958@users.noreply.github.com>
2026-06-01 23:50:13 +0900

    feat: Home page

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,15 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import tsParser from "@typescript-eslint/parser";
+
 export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
-    rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
     },
+  },
+  {
+    rules: {},
   },
 ];
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export type SocialLink = {
+  label: string;
+  url: string;
+  href: string;
+};
+
+export type Activity = {
+  date: string;
+  title: string;
+  href: string;
+};
