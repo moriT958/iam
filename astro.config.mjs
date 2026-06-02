dfe1c7ff01f57c81bae99e4b4350700770f0f90d
Morita <154768101+moriT958@users.noreply.github.com>
2026-06-02 18:46:03 +0900

    fix: rehype deprecated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { unified } from "@astrojs/markdown-remark";
 import rehypeHighlight from "rehype-highlight";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     syntaxHighlight: false,
-    rehypePlugins: [rehypeHighlight],
+    processor: unified({
+      rehypePlugins: [rehypeHighlight],
+    }),
   },
 });
